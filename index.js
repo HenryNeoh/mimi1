db6e4fde99699269d2b5d9b56cc4f336ce30eccd
HenryNeoh <149237972+HenryNeoh@users.noreply.github.com>
2024-05-12 14:10:00 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+﻿window.onload = function () {
+    var form = document.getElementById('settingsForm');
+
+    form.onsubmit = function (e) {
+        e.preventDefault();
+
+        var username = document.getElementById('username').value;
+        var email = document.getElementById('email').value;
+
+        localStorage.setItem('username', username);
+        localStorage.setItem('email', email);
+
+        alert('Settings saved!');
+
+        // Redirect to the next page
+        window.location.href = "new.html";
+    };
+};
